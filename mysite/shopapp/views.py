9999700004ff54ca5f9182d4d4db9667c138ad15
Алексей Павлов <pavlov.alexy@gmail.com>
2023-08-22 01:39:05 +0300

    Созданы модель "Order" и команды для нее

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -2,6 +2,7 @@ from timeit import default_timer
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from django.contrib.auth.models import Group
+from .models import Product, Order
 
 
 def shop_index(request: HttpRequest) -> render:
@@ -23,3 +24,17 @@ def groups_list(request: HttpRequest) -> render:
         "groups": Group.objects.prefetch_related('permissions').all(),
     }
     return render(request, 'shopapp/groups-list.html', context=context)
+
+
+def products_list(request: HttpRequest) -> render:
+    context = {
+        "products": Product.objects.all(),
+    }
+    return render(request, 'shopapp/products_list.html', context=context)
+
+
+def orders_list(request: HttpRequest) -> render:
+    context = {
+        "orders": Order.objects.select_related("user").prefetch_related('products').all(),
+    }
+    return render(request, 'shopapp/orders_list.html', context=context)
