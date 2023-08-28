eb2f39f1d8dff12494ed805a38b50eb1108f2b72
Алексей Павлов <pavlov.alexy@gmail.com>
2023-08-29 01:24:31 +0300

    Добавлено приложение "main" и шаблон для него.

---
+++ mysite/main/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+
+def main(request: HttpRequest) -> render:
+
+    return render(request, 'main/index.html')
+
